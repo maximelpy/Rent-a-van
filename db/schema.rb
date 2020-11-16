317c3cec53532040c8efac270e1ba9553463baa1
Alexandre Coëffet <alexandre.coeffet@hec.ca>
2020-11-16 16:52:07 -0500

    migration model van

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_16_211804) do
+ActiveRecord::Schema.define(version: 2020_11_16_214843) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,4 +27,17 @@ ActiveRecord::Schema.define(version: 2020_11_16_211804) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  create_table "vans", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.integer "price"
+    t.boolean "booked"
+    t.string "address"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_vans_on_user_id"
+  end
+
+  add_foreign_key "vans", "users"
 end
