ca1e54987f1a592d50030bc858be57eda322bdec
Alexandre Coëffet <alexandre.coeffet@hec.ca>
2020-11-16 17:32:07 -0500

    creation of the model booking

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_16_214843) do
+ActiveRecord::Schema.define(version: 2020_11_16_222733) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.bigint "van_id", null: false
+    t.date "start_date"
+    t.date "end_date"
+    t.bigint "user_id", null: false
+    t.boolean "approved"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+    t.index ["van_id"], name: "index_bookings_on_van_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -39,5 +51,7 @@ ActiveRecord::Schema.define(version: 2020_11_16_214843) do
     t.index ["user_id"], name: "index_vans_on_user_id"
   end
 
+  add_foreign_key "bookings", "users"
+  add_foreign_key "bookings", "vans"
   add_foreign_key "vans", "users"
 end
