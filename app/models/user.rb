a0812b1b8091644b3fbf9e3aed339a79bd0db115
Maxime Lapraye <71738109+maximelpy@users.noreply.github.com>
2020-11-17 11:43:51 -0500

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   has_many :bookings
   validates :email, format: { with: /\A.*@.*\.com\z/ }
   validates :age, inclusion: { greater_than_or_equal_to: 18 }, numericality: { only_integer: true }
-  validates :driving_licence_year, inclusion: { less_than_or_equal_to: 2004 }, numericality: { only_integer: true }
+  validates :driving_licence_year, numericality: { only_integer: true }
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
