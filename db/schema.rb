3edd5694b9c702cb319620ace20437d6a2f8e96c
Maxime Lapraye <maxime.lapraye@gmail.com>
2020-11-18 12:54:06 -0500

    map done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_17_174230) do
+ActiveRecord::Schema.define(version: 2020_11_18_160533) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,8 +45,7 @@ ActiveRecord::Schema.define(version: 2020_11_17_174230) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "age"
-    t.string "first_name"
-    t.string "last_name"
+    t.string "full_name"
     t.boolean "owner"
     t.integer "sign_in_count"
     t.datetime "current_sign_in_at"
@@ -66,6 +65,8 @@ ActiveRecord::Schema.define(version: 2020_11_17_174230) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_vans_on_user_id"
   end
 
