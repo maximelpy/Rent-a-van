86da4d81bd4f2a51e1ed3389c5a6281323b3f266
Alexandre Coëffet <alexandre.coeffet@hec.ca>
2020-11-19 13:39:54 -0500

    column approved changed (status)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_19_160001) do
+ActiveRecord::Schema.define(version: 2020_11_19_171243) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 2020_11_19_160001) do
     t.date "start_date"
     t.date "end_date"
     t.bigint "user_id", null: false
-    t.boolean "approved", default: false
+    t.string "approved", default: "pending"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_bookings_on_user_id"
